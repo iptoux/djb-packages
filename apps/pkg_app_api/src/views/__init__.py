ca1894687160ebebe0f4f6ddc8fcb5c64be53ec3
iptoux <donvito@kabelmail.de>
2025-01-08 22:59:52 +0100

    ✨ feat(API App): First init
    First init of first version from api application.
    
    src

---
+++ apps/pkg_app_api/src/views/__init__.py
@@ -0,0 +1,6 @@
+from .login import LoginView
+from .logout import LogoutView
+from .register import RegisterView
+from .user import UserView
+
+__all__ = ["LoginView", "LogoutView", "RegisterView", "UserView"]
---
+++ apps/pkg_app_api/src/views/logout.py
@@ -0,0 +1,13 @@
+import jwt, datetime
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
